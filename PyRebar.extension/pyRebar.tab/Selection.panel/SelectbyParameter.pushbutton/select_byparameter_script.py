f4a7a4b51fa1df27a2497bcfcfe82e575c99900d
Marcin Wolinski <mwo@woodthilsted.com>
2023-01-29 18:21:48 +0100

    Select by Parameter added.

--- PyRebar.extension/pyRebar.tab/Selection.panel/SelectbyParameter.pushbutton/select_byparameter_script.py
+++ PyRebar.extension/pyRebar.tab/Selection.panel/SelectbyParameter.pushbutton/select_byparameter_script.py
@@ -1,4 +1,4 @@
-__doc__ = "Select all rebars in view by Diameter."
+__doc__ = "Select all rebars in view by chosen Parameter."
 __title__ = "Select by Diameter"
 __author__ = "Wolinski"
 from Autodesk.Revit import DB
@@ -8,3 +8,10 @@ from Autodesk.Revit.DB import *
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
 view = doc.ActiveView
+
+"""
+Goal is to create gui window with lisbox of parameters. Some of them can be predefined such as Comment, Partition
+and add additional window with possibility to add own custom parameters and remove them.
+This will require creation of some sort of .db file. This is cool bigger project.
+A lot to learn
+"""

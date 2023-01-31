0d5992248c9de07e2894d4276deba47c08a48786
Marcin Wolinski <mwo@woodthilsted.com>
2023-01-31 17:16:04 +0100

    Reverse hook work.

--- PyRebar.extension/pyRebar.tab/Modify.panel/ReverseHook.pushbutton/reverse_hook_script.py
+++ PyRebar.extension/pyRebar.tab/Modify.panel/ReverseHook.pushbutton/reverse_hook_script.py
@@ -1,4 +1,6 @@
-__doc__ = "Reverse Hook at Start and Hook at End of the rebar."
+__doc__ = """Reverse 'Hook at Start' and 'Hook at End' of the rebar.
+
+Works only when 'Include hooks is Rebar Shape definition' option is disabled."""
 __title__ = "Reverse Hook"
 __author__ = "Wolinski"
 from Autodesk.Revit import DB
@@ -8,23 +10,49 @@ from Autodesk.Revit.DB import *
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
 view = doc.ActiveView
-t = Transaction(doc, "Set Length")
 
+# Get reinforcement settings
+settings = DB.Structure.ReinforcementSettings.GetReinforcementSettings(doc)
+defines_hooks = settings.RebarShapeDefinesHooks
+
+# Get selected object
 selection = [doc.GetElement(x) for x in uidoc.Selection.GetElementIds()]
+# TODO: If no object is selected prompt user to select
 
+# Handling transactions
 tg = TransactionGroup(doc, "ReverseHook")
 tg.Start()
 
+
+def can_reverse(rebar):
+    if rebar.IsShapeDriven and defines_hooks:
+        return False
+    elif rebar.IsFreeForm:
+        return True
+    else:
+        print("Unknown rebar type")
+
+
 # TODO: Make this flexible and working for single bars and rebar groups.
+# TODO: Add check if RebarShapeDefinesHooks is true or false, if true messagebox user that function will not work.
 for rebar in selection:
+
     orient0 = rebar.GetHookOrientation(0)
     orient1 = rebar.GetHookOrientation(1)
-
-    t = Transaction(doc, "SubTransaction")
+    left = DB.Structure.RebarHookOrientation.Left
+    right = DB.Structure.RebarHookOrientation.Right
+    print(orient0)
+    print(orient1)
+    t = Transaction(doc, "Reverse")
     t.Start()
-
-    if orient0 == 0:
-        rebar.SetHookOrientation(0, DB.Structure.RebarHookOrientation.Left)
-        rebar.SetHookOrientation(1, DB.Structure.RebarHookOrientation.Left)
+    if orient0 == right:
+        rebar.SetHookOrientation(0, left)
+    elif orient0 == left:
+        rebar.SetHookOrientation(0, right)
+    if orient1 == right:
+        rebar.SetHookOrientation(1, left)
+    elif orient1 == left:
+        rebar.SetHookOrientation(1, right)
     t.Commit()
+
 tg.Assimilate()

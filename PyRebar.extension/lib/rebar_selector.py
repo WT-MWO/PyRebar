3e5470e1dd7d8ffcd3500c8b70f046ed59c06d9f
Marcin Wolinski <mwo@woodthilsted.com>
2024-09-19 17:29:20 +0200

    Rebar selector bugfix

--- PyRebar.extension/lib/rebar_selector.py
+++ PyRebar.extension/lib/rebar_selector.py
@@ -1,5 +1,6 @@
 from Autodesk.Revit.UI import *
 from Autodesk.Revit import DB
+from Autodesk.Revit import UI
 
 
 class RebarSelector:
@@ -9,10 +10,7 @@ class RebarSelector:
 
     def is_rebar(self, obj):
         """Checks if object is Rebar type."""
-        # for obj in selection:
-        #     return obj.Category.IsBuiltInCategoryValid(DB.BuiltInCategory.OST_Rebar)
-        # for obj in selection:
-        return obj.Category.IsBuiltInCategoryValid(DB.BuiltInCategory.OST_Rebar)
+        return obj.Category.BuiltInCategory == DB.BuiltInCategory.OST_Rebar
 
     def is_rebar_group(rebar_object):
         n_bars = rebar_object.NumberOfBarPositions
@@ -21,22 +19,30 @@ class RebarSelector:
         else:
             return False
 
+    def return_rebars(self, selection):
+        elements = []
+        for e in selection:
+            if self.is_rebar(e):
+                elements.append(e)
+        return elements
+
     def get_rebars(self):
         """Gets Rebar type objects from selected elements, if no objects selected
         triggers Pick Objects method"""
-        elements = []
+        # elements = []
         selection = [self.doc.GetElement(x) for x in self.uidoc.Selection.GetElementIds()]
         # If no object is selected prompt user to select
         if len(selection) < 1:
-            selected_obj = self.uidoc.Selection.PickObjects(Selection.ObjectType.Element, "Choose rebars")
-            selection = [self.doc.GetElement(el_id) for el_id in selected_obj]
-        elif len(selection) == 1:
-            elements.append(selection[0])
-        else:
-            for e in selection:
-                if self.is_rebar(e):
-                    elements.append(e)
-        return elements
+            selected_obj = self.uidoc.Selection.PickObjects(UI.Selection.ObjectType.Element, "Choose rebars")
+            selection = [self.doc.GetElement(reference.ElementId) for reference in selected_obj]
+        return self.return_rebars(selection)
+        # elif len(selection) == 1:
+        #     elements.append(selection[0])
+        # else:
+        #     for e in selection:
+        #         if self.is_rebar(e):
+        #             elements.append(e)
+        # return elements
 
     def get_all_rebars(self):
         """Gets Rebar type objects from selected elements, if no objects selected

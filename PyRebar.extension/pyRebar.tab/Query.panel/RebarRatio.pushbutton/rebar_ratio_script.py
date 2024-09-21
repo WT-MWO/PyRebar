c77e73bbd8fc3658e612d31332d66bff76558938
Marcin Wolinski <mwo@woodthilsted.com>
2024-09-21 15:24:08 +0200

    Rebar ratio script implemented

--- PyRebar.extension/pyRebar.tab/Query.panel/RebarRatio.pushbutton/rebar_ratio_script.py
+++ PyRebar.extension/pyRebar.tab/Query.panel/RebarRatio.pushbutton/rebar_ratio_script.py
@@ -1,4 +1,5 @@
-__doc__ = "Calculate rebar weight to concrete volume ratio."
+__doc__ = "Calculate rebar weight to concrete volume ratio. If multiple elements are selected, ratio is computed as sum.\
+        You can pre-select element or pick after script is triggered."
 __title__ = "Rebar ratio"
 __author__ = "MWolinski"
 from Autodesk.Revit import DB
@@ -11,6 +12,9 @@ from rebar_selector import RebarSelector
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
 view = doc.ActiveView
+
+# TODO: if user selects element and rebars, ignore rebars, filter only valid formwork
+# TODO: if no valid elements in selection raise error
 
 
 def get_objects(doc, uidoc):
@@ -38,15 +42,13 @@ def can_host_rebar(element):
         return False
 
 
-filter = DB.ElementClassFilter(DB.Structure.Rebar)
-
-
 def get_element_dependent_rebars(element):
     """Finds all dependent rebars of the element:
     Arguments:
         element(Element)
     Returns:
         list[Rebar]"""
+    filter = DB.ElementClassFilter(DB.Structure.Rebar)
     dependent_elements = element.GetDependentElements(filter)
     rebars = []
     for id in dependent_elements:
@@ -61,9 +63,15 @@ def get_total_rebar_mass(rebars):
         rebars(list[Rebar])
     Returns:
         mass(float) - mass in kg"""
+    masses = []
     for rebar in rebars:
         # do stuff
-        pass
+        R_O = 7.85e-3  # kg/cm3
+        FEET3_TO_CM3 = 28316.832082557
+        volume = rebar.Volume * FEET3_TO_CM3  # basic value comes as feet^3
+        mass = volume * R_O
+        masses.append(mass)
+    return sum(masses)
 
 
 def calculate_ratio(element, rebars):
@@ -72,24 +80,40 @@ def calculate_ratio(element, rebars):
         element(Element)
         rebars(list[Rebar])
     Returns:
-        ratio(float)"""
-    # get element volume
-    volume = element.Volume
-    # get all element rebars
+        list[volume(float) in m3,total_rebar_mass(float) in kg, ratio(float) in kg/m3"""
+    FEET3_TO_M3 = 0.02831683208256
+    volume = (
+        element.get_Parameter(DB.BuiltInParameter.HOST_VOLUME_COMPUTED).AsDouble() * FEET3_TO_M3
+    )  # basic value comes as feet^3
+    if volume == 0:
+        raise ZeroDivisionError
     total_rebar_mass = get_total_rebar_mass(rebars)
     ratio = round(total_rebar_mass / volume, 2)
-    print(ratio)
-    # TODO: add more info, like how many elements included?
+    return [volume, total_rebar_mass, ratio]
 
 
 # Main logic
 elements = get_objects(doc, uidoc)
 if len(elements) > 1:
+    volumes = []
+    rebar_masses = []
     for element in elements:
         if can_host_rebar(element):
-            # do stuff
-            pass
+            rebars = get_element_dependent_rebars(element)
+            volume, total_mass, ratio = calculate_ratio(element, rebars)
+            volumes.append(volume)
+            rebar_masses.append(total_mass)
+    total_volume = sum(volumes)
+    total_rc_mass = sum(rebar_masses)
+    total_ratio = round(total_rc_mass / total_volume, 2)
+    print("Concrete volume: {} m3".format(total_volume))
+    print("Total rebar mass: {} kg".format(total_rc_mass))
+    print("Ratio: {} kg/m3".format(total_ratio))
+
 elif len(elements) == 1:
     if can_host_rebar(elements[0]):
         rebars = get_element_dependent_rebars(elements[0])
-        pass
+        volume, total_rc_mass, ratio = calculate_ratio(elements[0], rebars)
+        print("Concrete volume: {} m3".format(volume))
+        print("Total rebar mass: {} kg".format(total_rc_mass))
+        print("Ratio: {} kg/m3".format(ratio))
